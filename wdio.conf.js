35d319081445df679a322fd8e859d38a0fcb5dde
byju-Sushil <121926601+byju-Sushil@users.noreply.github.com>
2023-06-12 17:46:53 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ export const config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/first.js'
         // './test/specs/practice.js'
     ],
     // Patterns to exclude.
